c3963a6fd76debf81aeebc564a8f18121231046f
da99 <i-hate-spam-45671204@mailinator.com>
2012-01-05 03:53:30 -0600

    Added: spec for Server_Group.new('*')

--- spec/tests/Remote_Server_Group.rb
+++ spec/tests/Remote_Server_Group.rb
@@ -8,4 +8,11 @@ describe "Server_Group" do
     }
   end
   
+  it 'grabs all servers for group "*"' do
+    Dir.chdir('spec/Boxes') {
+      group = Unified_IO::Remote::Server_Group.new("*")
+      group.servers.map(&:hostname).sort.should == %w{ db1 s1 s2 }
+    }
+  end
+  
 end # === describe Server_Group
