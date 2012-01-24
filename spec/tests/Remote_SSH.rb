ddf5e7f1d851097426c2507a16d31b99dd6413d8
da99 <i-hate-spam-45671204@mailinator.com>
2012-01-24 13:19:26 -0600

    Specs: separated specs for :spec_exec and :spec_run

--- spec/tests/Remote_SSH.rb
+++ spec/tests/Remote_SSH.rb
@@ -1,5 +1,5 @@
 
-describe "Remote SSH" do
+describe ":ssh_exec" do
   
   behaves_like 'SSH to local'
   
@@ -14,7 +14,7 @@ describe "Remote SSH" do
   it "raises Wrong_IP when hostnames do not match" do
     lambda {
       self.server = @wrong_ip
-      ssh_run 'uptime'
+      ssh_exec 'uptime'
     }.should.raise(Unified_IO::Remote::SSH::Wrong_IP)
     .message.match %r!Hostname: localhost, Target: !
   end
@@ -24,21 +24,16 @@ describe "Remote SSH" do
       lambda {
         ENV['SKIP_IP_CHECK'] = 'true'
         self.server = @wrong_ip
-        ssh_run 'hostname'
+        ssh_exec 'hostname'
       }.should.not.raise(Unified_IO::Remote::SSH::Wrong_IP)
     ensure
       ENV.delete 'SKIP_IP_CHECK'
     end
   end
-
-  it "strips returned data" do
-    target = `uptime`.strip.gsub(%r!\d+!, '[0-9]{1,2}')
-    ssh_run("uptime").should.match %r!#{target}!
-  end
   
   it 'raises SSH::Exit_Error if return status is not zero' do
     e = lambda {
-      ssh_run "HOSTNAMES"
+      ssh_exec "HOSTNAMES"
     }.should.raise(Unified_IO::Remote::SSH::Exit_Error)
 
     e.result.exit_status.should == 127
@@ -48,11 +43,27 @@ describe "Remote SSH" do
     ssh_exec("hostname").should.be.is_a Unified_IO::Remote::SSH::Results
   end
   
+  it "strips returned data" do
+    target = `uptime`.strip.gsub(%r!\d+!, '[0-9]{1,2}')
+    ssh_exec("uptime").data.join.should.match %r!#{target}!
+  end
+  
   it 'returns a SSH::Results with an array of data' do
     ssh_exec("whoami").data.should.be == [`whoami`.strip]
   end
+
+end # === describe :ssh_exec
+
+describe ":ssh_run" do
   
-end # === describe Remote SSH
-              
+  behaves_like 'SSH to local'
+  
+  it "returns a stripped String" do
+    target = `uptime`.strip.gsub(%r!\d+!, '[0-9]{1,2}')
+    ssh_run("uptime").should.match %r!#{target}!
+  end
+
+end # === describe :ssh_run
+           
   
   
