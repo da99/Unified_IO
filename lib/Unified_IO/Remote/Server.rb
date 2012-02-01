9302db16ebd144d5e8e6d243b18b31e8629ae43f
da99 <i-hate-spam-45671204@mailinator.com>
2012-02-01 12:04:54 -0600

    Fixed: Remote::Server has a Base module

--- lib/Unified_IO/Remote/Server.rb
+++ lib/Unified_IO/Remote/Server.rb
@@ -15,58 +15,64 @@ module Unified_IO
         :custom
       ]
 
-      attr_reader :origin, *PROPS
-      attr_accessor :os_name
+      module Base
 
-      def initialize hash, opts = {}
-        raise ArgumentError, "Argument not a hash: #{hash.inspect}" unless hash.is_a?(Hash)
+        attr_reader :origin, *PROPS
+        attr_accessor :os_name
 
-        @custom = opts[:custom] || []
-        valid_keys = custom + PROPS
-        invalid = hash.keys - valid_keys
-        raise Invalid_Property, "Invalid keys: #{invalid.inspect}" unless invalid.empty?
+        def initialize hash, opts = {}
+          raise ArgumentError, "Argument not a hash: #{hash.inspect}" unless hash.is_a?(Hash)
 
-        if hash.has_key?(:password) && hash[:password].strip.empty?
-          raise Invalid_Property, ":password can't be set as empty string."
-        end
-        
-        custom.each { |key|
-          raise Invalid_Property, "#{key.inspect}" unless key.is_a?(Symbol)
-          raise Invalid_Property, "#{key.inspect} already defined" if respond_to?(key)
-          raise Missing_Property, "#{key.inspect}" unless hash.has_key?(key)
-        }
+          @custom = opts[:custom] || []
+          valid_keys = custom + PROPS
+          invalid = hash.keys - valid_keys
+          raise Invalid_Property, "Invalid keys: #{invalid.inspect}" unless invalid.empty?
 
-        if ENV['PASSWORD']
-          hash[:password] = ENV['PASSWORD']
-          hash[:login] = 'root'
-        end
+          if hash.has_key?(:password) && hash[:password].strip.empty?
+            raise Invalid_Property, ":password can't be set as empty string."
+          end
 
-        @origin = hash
+          custom.each { |key|
+            raise Invalid_Property, "#{key.inspect}" unless key.is_a?(Symbol)
+            raise Invalid_Property, "#{key.inspect} already defined" if respond_to?(key)
+            raise Missing_Property, "#{key.inspect}" unless hash.has_key?(key)
+          }
 
-        origin.keys.each { |key|
-          instance_variable_set :"@#{key}", origin[key] if valid_keys.include?(key)
-          
-          if custom.include?(key)
-            eval %~
+          if ENV['PASSWORD']
+            hash[:password] = ENV['PASSWORD']
+            hash[:login] = 'root'
+          end
+
+          @origin = hash
+
+          origin.keys.each { |key|
+            instance_variable_set :"@#{key}", origin[key] if valid_keys.include?(key)
+
+            if custom.include?(key)
+              eval %~
               def self.#{key}
                 @#{key}
               end
             ~, nil, __FILE__, __LINE__ - 3 
+            end
+          }
+
+          @port ||= '22'
+          @port = Integer(@port)
+
+          if !hostname.is_a?(String)
+            raise Invalid_Property, "Invalid hostname: #{hostname.inspect}"
           end
-        }
 
-        @port ||= '22'
-        @port = Integer(@port)
+          @ip    ||= @hostname
+          @user  ||= @login
+          @login ||= @user
 
-        if !hostname.is_a?(String)
-          raise Invalid_Property, "Invalid hostname: #{hostname.inspect}"
-        end
+        end # === def initialize
 
-        @ip    ||= @hostname
-        @user  ||= @login
-        @login ||= @user
-
-      end # === def initialize
+      end # === module Base
+      
+      include Base
 
     end # === class Server
   end # === module Remote
